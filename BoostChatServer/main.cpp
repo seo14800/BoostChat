1af8990e94f691063e57248905298fe4f339c659
seo14800 <seo14800@gmail.com>
2024-05-13 16:58:54 +0900

    modify main

--- BoostChatServer/main.cpp
+++ BoostChatServer/main.cpp
@@ -1,9 +1,20 @@
-#include <boost/regex.hpp>
 #include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include "chat_server.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
-	boost::regex pattern("a.c");
-	std::cout << "Hello, World!" << std::endl;
+	boost::asio::io_context ioContext;
+
+	std::cout << "Boost chat server ..." << std::endl;
+
+	if (argc < 2)
+	{
+		std::cout << "need a argv (port)" << std::endl;
+		return -1;
+	}
+
+
 	return 0;
 }
